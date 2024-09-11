262d88b74c40e65b354a6355e1eefd0684c3ba7f
Chaekang Lee <leechis7@gmail.com>
2024-09-11 14:32:15 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js! by leechis</h1>
     </main>
   );
 }
